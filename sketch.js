19edd89910535e29b8d05c43f81ec43fdf481852
Yishak Wondim <it.yisak.wondim@gmail.com>
2022-09-13 08:56:01 +0300

    drawing grid finished

--- sketch.js
+++ sketch.js
@@ -1,28 +1,32 @@
+const sketchArea = document.querySelector(".sketch-area");
+const gridNumber = document.querySelector("#grid-size");
 
 
-
-const  sketchArea = document.querySelector('.sketch-area');
-const gridNumber = document.querySelector('#grid-size');
-
-
-
-function drawGrid(){
- var numberOfGrid = gridNumber.value;
- 
- 
- for (let i = 1 ; i <= numberOfGrid ** 2; i++){
-    let gridItem = document.createElement('div');
-    gridItem.classList.add('grid-item');
-    sketchArea.appendChild(gridItem);
- }
- let gridItem = document.querySelector('.grid-item');
- gridItem.style.border = '1px solid grey';
- sketchArea.setAttribute('style', `display:grid;grid-template-columns:repeat(${numberOfGrid},1fr);grid-template-rows:repeat(${numberOfGrid}, 1fr); `)
- sketchArea.style['grid-template-columns']= `repeat(${numberOfGrid}), 1fr`;
- sketchArea.style['grid-template-rows']= `repeat(${numberOfGrid}), 1fr`;
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+  }
 }
+function drawGrid() {
 
+  removeAllChildNodes(sketchArea);
+  var numberOfGrid = gridNumber.value;
+  for (let i = 1; i <= numberOfGrid; i++) {
+    for (let j = 1; j <= numberOfGrid; j++) {
+      
+      let gridItem = document.createElement("div");
+      gridItem.classList.add("grid-item");
+      sketchArea.appendChild(gridItem);
+      let gridItems = document.querySelectorAll('.grid-item');
+      let count = gridItems.length;
+      console.log(count);
+    }
 
-
-
-
+  }
+  sketchArea.setAttribute(
+    "style",
+    `display:grid;grid-template-columns:repeat(${numberOfGrid},1fr);grid-template-rows:repeat(${numberOfGrid}, 1fr); `
+  );
+  sketchArea.style["grid-template-columns"] = `repeat(${numberOfGrid}), 1fr`;
+  sketchArea.style["grid-template-rows"] = `repeat(${numberOfGrid}), 1fr`;
+}
