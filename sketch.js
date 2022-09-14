7d2123958187d5d46479ebbf1f5450a8d30f8ead
yisha17 <it.yisak.wondim@gmail.com>
2022-09-14 03:48:17 +0300

    color input decorated

--- sketch.js
+++ sketch.js
@@ -4,16 +4,15 @@ const gridNumber = document.querySelector("#grid-size");
 
 function removeAllChildNodes(parent) {
   while (parent.firstChild) {
-      parent.removeChild(parent.firstChild);
+    parent.removeChild(parent.firstChild);
   }
 }
-function drawGrid() {
 
+function drawGrid() {
   removeAllChildNodes(sketchArea);
   var numberOfGrid = gridNumber.value;
   for (let i = 1; i <= numberOfGrid; i++) {
     for (let j = 1; j <= numberOfGrid; j++) {
-      
       let gridItem = document.createElement("div");
       gridItem.classList.add("grid-item");
       sketchArea.appendChild(gridItem);
@@ -25,82 +24,82 @@ function drawGrid() {
   );
   sketchArea.style["grid-template-columns"] = `repeat(${numberOfGrid}), 1fr`;
   sketchArea.style["grid-template-rows"] = `repeat(${numberOfGrid}), 1fr`;
-  shading();
+  oneColor();
 }
 
 drawGrid();
 
-function chooseColor(grid){
-  let colorInput = document.querySelector('#color');
+function chooseColor(grid) {
+  let colorInput = document.querySelector("#color");
   let colorChoice = colorInput.value;
   return colorChoice;
 }
 
 
-
-function drawGrid(){
- var numberOfGrid = gridNumber.value;
- 
- 
- for (let i = 1 ; i <= numberOfGrid ** 2; i++){
-    let gridItem = document.createElement('div');
-    gridItem.classList.add('grid-item');
-    sketchArea.appendChild(gridItem);
- }
- let gridItem = document.querySelector('.grid-item');
- sketchArea.setAttribute('style', `display:grid;grid-template-columns:repeat(${numberOfGrid},1fr);grid-template-rows:repeat(${numberOfGrid}, 1fr); `)
- sketchArea.style['grid-template-columns']= `repeat(${numberOfGrid}), 1fr`;
- sketchArea.style['grid-template-rows']= `repeat(${numberOfGrid}), 1fr`;
-
-function oneColor(){
-  let gridItem = document.querySelectorAll('.grid-item');
-  gridItem.forEach((grid) => {
-    grid.addEventListener('mouseover', function(e)  {
-      e.target.setAttribute('style',`background-color: ${chooseColor()}`)
+  function oneColor() {
+    let gridItem = document.querySelectorAll(".grid-item");
+    gridItem.forEach((grid) => {
+      grid.addEventListener("mouseover", function (e) {
+        e.target.setAttribute("style", `background-color: ${chooseColor()}`);
+      });
     });
-  })
+  }
+
+  function randomizecolor(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function randomColor() {
+    let gridItem = document.querySelectorAll(".grid-item");
+
+    gridItem.forEach((grid) => {
+      grid.addEventListener("mouseover", function (e) {
+        e.target.setAttribute(
+          "style",
+          `background-color: rgb(${randomizecolor(
+            0,
+            255
+          ).toString()},${randomizecolor(0, 255).toString()},${randomizecolor(
+            0,
+            255
+          ).toString()})`
+        );
+      });
+    });
+  }
+function shading() {
+  let gridItem = document.querySelectorAll(".grid-item");
+  gridItem.forEach((grid) => {
+    grid.setAttribute("style", "background-color: rgb(215,255,255)");
+    grid.addEventListener("mouseover", darken);
+  });
+}
+  function darken(e) {
+    let oldColor = e.target.style.backgroundColor;
+    let singleColor = oldColor.slice(4, 9);
+    let currentColor = parseInt(singleColor) - 15;
+
+    if (currentColor <= 0) return;
+    else {
+      let colorString = currentColor.toString();
+      return (e.target.style.backgroundColor = `rgb(${colorString},${colorString},${colorString})`);
+    }
+  }
+
+
+function clearGrid() {
+  let gridItem = document.querySelectorAll(".grid-item");
+
+  gridItem.forEach((grid) => {
+    grid.style.backgroundColor = "rgb(255,255,255)";
+  });
 }
 
-function randomizecolor(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function randomColor(){
-  let gridItem = document.querySelectorAll('.grid-item');
-
+function eraser(){
+  let gridItem = document.querySelectorAll(".grid-item");
   gridItem.forEach((grid) => {
     grid.addEventListener('mouseover', function(e){
-      e.target.setAttribute('style', `background-color: rgb(${randomizecolor(0,255).toString()},${randomizecolor(0,255).toString()},${randomizecolor(0,255).toString()})`)
+      e.target.style.backgroundColor = 'rgb(255,255,255)';
     })
   })
 }
-
-
-
-function shading(){
-  let gridItem = document.querySelectorAll('.grid-item');
-  gridItem.forEach((grid) => {
-    grid.setAttribute('style', 'background-color: rgb(215,255,255)');
-    grid.addEventListener('mouseover', darken )
-  })
-}
-
-function darken(e) {
-  let oldColor = e.target.style.backgroundColor;
-   let singleColor = oldColor.slice(4,9)
-   let currentColor = parseInt(singleColor) - 15;
-
-   if(currentColor <= 0) return;
-   else{
-    let colorString = currentColor.toString();
-    return e.target.style.backgroundColor = `rgb(${colorString},${colorString},${colorString})`
-   }
-   
-   
-  
-
-}
-}
-
-
-
